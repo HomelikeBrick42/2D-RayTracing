b34764c57725998321f10de55d2fa46ff534f716
HomelikeBrick42 <64717463+HomelikeBrick42@users.noreply.github.com>
2023-11-02 19:21:24 +1300

    Added camera and player position

--- src/lib.rs
+++ src/lib.rs
@@ -2,13 +2,25 @@ use eframe::{
     egui,
     wgpu::{self, include_wgsl},
 };
+use encase::{ShaderSize, ShaderType, UniformBuffer};
+
+#[derive(ShaderType)]
+struct GpuCamera {
+    position: cgmath::Vector2<f32>,
+    height: f32,
+    player_position: cgmath::Vector2<f32>,
+}
 
 pub struct App {
     egui_texture_id: egui::TextureId,
     main_texture: wgpu::Texture,
     output_texture_bind_group_layout: wgpu::BindGroupLayout,
     output_texture_bind_group: wgpu::BindGroup,
+    camera: GpuCamera,
+    camera_buffer: wgpu::Buffer,
+    camera_bind_group: wgpu::BindGroup,
     compute_pipeline: wgpu::ComputePipeline,
+    camera_window: bool,
 }
 
 impl App {
@@ -60,10 +72,41 @@ impl App {
             }],
         });
 
+        let camera_buffer = device.create_buffer(&wgpu::BufferDescriptor {
+            label: Some("Camera Buffer"),
+            size: <GpuCamera as ShaderSize>::SHADER_SIZE.get(),
+            usage: wgpu::BufferUsages::COPY_DST | wgpu::BufferUsages::UNIFORM,
+            mapped_at_creation: false,
+        });
+
+        let camera_bind_group_layout =
+            device.create_bind_group_layout(&wgpu::BindGroupLayoutDescriptor {
+                label: Some("Camera Bind Group Layout"),
+                entries: &[wgpu::BindGroupLayoutEntry {
+                    binding: 0,
+                    visibility: wgpu::ShaderStages::COMPUTE,
+                    ty: wgpu::BindingType::Buffer {
+                        ty: wgpu::BufferBindingType::Uniform,
+                        has_dynamic_offset: false,
+                        min_binding_size: Some(<GpuCamera as ShaderSize>::SHADER_SIZE),
+                    },
+                    count: None,
+                }],
+            });
+
+        let camera_bind_group = device.create_bind_group(&wgpu::BindGroupDescriptor {
+            label: Some("Camera Bind Group"),
+            layout: &camera_bind_group_layout,
+            entries: &[wgpu::BindGroupEntry {
+                binding: 0,
+                resource: camera_buffer.as_entire_binding(),
+            }],
+        });
+
         let compute_pipeline_layout =
             device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
                 label: Some("Compute Pipeline Layout"),
-                bind_group_layouts: &[&output_texture_bind_group_layout],
+                bind_group_layouts: &[&output_texture_bind_group_layout, &camera_bind_group_layout],
                 push_constant_ranges: &[],
             });
 
@@ -84,25 +127,80 @@ impl App {
             main_texture,
             output_texture_bind_group_layout,
             output_texture_bind_group,
+            camera: GpuCamera {
+                position: cgmath::vec2(0.0, 0.0),
+                height: 1.0,
+                player_position: cgmath::vec2(0.0, 0.0),
+            },
+            camera_buffer,
+            camera_bind_group,
             compute_pipeline,
+            camera_window: false,
         }
     }
 }
 
 impl eframe::App for App {
     fn update(&mut self, ctx: &eframe::egui::Context, frame: &mut eframe::Frame) {
-        let eframe::egui_wgpu::RenderState {
-            device,
-            queue,
-            renderer,
-            ..
-        } = frame.wgpu_render_state().unwrap();
+        egui::TopBottomPanel::top("Top Panel").show(ctx, |ui| {
+            ui.horizontal(|ui| {
+                self.camera_window |= ui.button("Camera").clicked();
+            });
+        });
+
+        egui::Window::new("Camera")
+            .open(&mut self.camera_window)
+            .show(ctx, |ui| {
+                ui.horizontal(|ui| {
+                    ui.label("Position: ");
+                    ui.add(
+                        egui::DragValue::new(&mut self.camera.position.x)
+                            .prefix("x:")
+                            .speed(0.01),
+                    );
+                    ui.add(
+                        egui::DragValue::new(&mut self.camera.position.y)
+                            .prefix("y:")
+                            .speed(0.01),
+                    );
+                });
+
+                ui.horizontal(|ui| {
+                    ui.label("View Height: ");
+                    ui.add(egui::DragValue::new(&mut self.camera.height).speed(0.1));
+                    self.camera.height = self.camera.height.max(0.01);
+                });
+
+                ui.horizontal(|ui| {
+                    ui.label("Player Position: ");
+                    ui.add(
+                        egui::DragValue::new(&mut self.camera.player_position.x)
+                            .prefix("x:")
+                            .speed(0.01),
+                    );
+                    ui.add(
+                        egui::DragValue::new(&mut self.camera.player_position.y)
+                            .prefix("y:")
+                            .speed(0.01),
+                    );
+                });
+
+                ui.allocate_space(ui.available_size());
+            });
 
         egui::CentralPanel::default()
             .frame(egui::Frame::none().fill(egui::Color32::from_rgb(255, 0, 255)))
             .show(ctx, |ui| {
+                let eframe::egui_wgpu::RenderState {
+                    device,
+                    queue,
+                    renderer,
+                    ..
+                } = frame.wgpu_render_state().unwrap();
+
                 let (_, rect) = ui.allocate_space(ui.available_size());
 
+                // Resize output texture if needed
                 let (width, height) = (rect.width() as i64, rect.height() as i64);
                 if self.main_texture.width() as i64 != width
                     && self.main_texture.height() as i64 != height
@@ -148,6 +246,15 @@ impl eframe::App for App {
                         });
                 }
 
+                // Upload camera uniform
+                {
+                    let mut buffer =
+                        UniformBuffer::new([0; <GpuCamera as ShaderSize>::SHADER_SIZE.get() as _]);
+                    buffer.write(&self.camera).unwrap();
+                    let buffer = buffer.into_inner();
+                    queue.write_buffer(&self.camera_buffer, 0, &buffer);
+                }
+
                 let mut command_encoder =
                     device.create_command_encoder(&wgpu::CommandEncoderDescriptor {
                         label: Some("Compute Command Encoder"),
@@ -166,6 +273,7 @@ impl eframe::App for App {
 
                     compute_pass.set_pipeline(&self.compute_pipeline);
                     compute_pass.set_bind_group(0, &self.output_texture_bind_group, &[]);
+                    compute_pass.set_bind_group(1, &self.camera_bind_group, &[]);
                     compute_pass.dispatch_workgroups(workgroups_x, workgroups_y, 1);
                 }
                 queue.submit([command_encoder.finish()]);
@@ -173,7 +281,7 @@ impl eframe::App for App {
                 ui.painter().image(
                     self.egui_texture_id,
                     rect,
-                    egui::Rect::from_min_max(egui::pos2(0.0, 0.0), egui::pos2(1.0, 1.0)),
+                    egui::Rect::from_min_max(egui::pos2(0.0, 1.0), egui::pos2(1.0, 0.0)),
                     egui::Color32::WHITE,
                 );
             });
--- src/shader.wgsl
+++ src/shader.wgsl
@@ -2,6 +2,16 @@
 @binding(0)
 var output_texture: texture_storage_2d<rgba8unorm, write>;
 
+struct Camera {
+    position: vec2<f32>,
+    height: f32,
+    player_position: vec2<f32>,
+}
+
+@group(1)
+@binding(0)
+var<uniform> camera: Camera;
+
 @compute
 @workgroup_size(16, 16)
 fn main(
@@ -14,5 +24,10 @@ fn main(
         return;
     }
 
-    textureStore(output_texture, coords, vec4<f32>(1.0, 0.0, 0.0, 1.0));
+    let aspect = f32(size.x) / f32(size.y);
+    let uv = vec2<f32>(coords) / vec2<f32>(size);
+
+    let world_position = (uv - 0.5) * vec2<f32>(aspect * camera.height, camera.height) + camera.position;
+
+    textureStore(output_texture, coords, clamp(vec4<f32>(world_position - camera.player_position, 0.0, 1.0), vec4<f32>(0.0), vec4<f32>(1.0)));
 }
